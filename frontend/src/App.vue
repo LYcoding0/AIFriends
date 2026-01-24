475c408985aa4175c27bd93ad89744e61c595d48
LYcoding0 <wuxi_liuying2001@163.com>
2026-01-24 23:09:43 +0800

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from "@/components/navbar/NavBar.vue";
+</script>
+
+<template>
+  <NavBar>
+    <h1>一个伟大的开端</h1>
+  </NavBar>
+</template>
+
+<style scoped>
+
+</style>
