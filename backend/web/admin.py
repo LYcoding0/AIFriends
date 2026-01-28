5aa3a2750ec31664cb666c63c19f40332770f9ae
LYcoding0 <wuxi_liuying2001@163.com>
2026-01-28 23:12:05 +0800

    实现数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from web.models.user import UserProfile
+
+
+@admin.register(UserProfile)  # 将UserProfile模型注册到Django管理后台
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)  # 列表页显示字段
