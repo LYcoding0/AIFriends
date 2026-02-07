7372832423b3ba5039f8d4669d2e4ffb620e8286
LYcoding0 <wuxi_liuying2001@163.com>
2026-02-08 00:24:28 +0800

    实现角色curd api

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
+# 先导入模型
 from web.models.user import UserProfile
+from web.models.character import Character
 
 
 @admin.register(UserProfile)  # 将UserProfile模型注册到Django管理后台
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)  # 列表页显示字段
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
---
+++ backend/web/views/create/character/update.py
@@ -0,0 +1,52 @@
+from django.utils.timezone import now
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from web.models.character import Character
+from web.views.utils.photo import remove_old_photo
+
+
+class UpdateCharacterView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            character_id = request.data['character_id']
+            # character_id字符串类型会自动转为int类型，__user是UserProfile的属性
+            character = Character.objects.get(id=character_id, author__user=request.user)
+            name = request.data['name'].strip()
+            profile = request.data['profile'].strip()
+            photo = request.FILES.get('photo', None)
+            background_image = request.FILES.get('background_image', None)
+
+            if not name:
+                return Response({
+                    'result': '请输入名称'
+                })
+            if not profile:
+                return Response({
+                    'result': '请输入简介'
+                })
+
+            if photo:
+                remove_old_photo(character.photo)
+                character.photo = photo
+
+            if background_image:
+                remove_old_photo(character.background_image)
+                character.background_image = background_image
+
+            character.name = name
+            character.profile = profile
+            character.updated_time = now()
+
+            character.save()
+            return Response({
+                'result': 'success'
+            })
+
+        except:
+            return Response({
+                'result': '系统异常，请稍后重试'
+            })
